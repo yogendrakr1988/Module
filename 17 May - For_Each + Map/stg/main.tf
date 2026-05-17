635e6a4b5d9d724f6f0439e5ee14acddace039c5
yogendrakr1988 <yogendraidea@gmail.com>
2026-05-17 13:23:43 +0530

    for_each & Map

---
+++ 17 May - For_Each + Map/stg/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_storage_account" "stg" {
+  for_each                 = var.tatastg
+  name                     = each.value.name
+  resource_group_name      = each.value.resource_group_name
+  location                 = each.value.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
+
